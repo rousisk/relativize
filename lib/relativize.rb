59a00ebe533b01f9b60b629122e0ae8cc182e428
Konstantinos Rousis <rousisk@gmail.com>
2015-08-07 15:00:19 +0200

    enable URI conversion to relative only (path + query)

--- lib/relativize.rb
+++ lib/relativize.rb
@@ -1,5 +1,22 @@
 require "relativize/version"
 
 class Relativize
-  # Your code goes here...
+  def initialize(url, fallback_url: '/')
+    @url = url
+    @fallback_url = fallback_url
+  end
+
+  def call
+    make_relative(@url)
+  end
+
+  private
+
+  def make_relative(url)
+    uri = URI.parse(url)
+
+    uri.query ? "#{uri.path}?#{uri.query}" : uri.path
+  rescue URI::Error
+    @fallback_url
+  end
 end
--- spec/relativize_spec.rb
+++ spec/relativize_spec.rb
@@ -5,7 +5,49 @@ describe Relativize do
     expect(Relativize::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'given an absolute URL' do
+    it 'returns only the path and the query' do
+      url = 'https://example:80/path/more?query=1&param=true'
+
+      expect(Relativize.new(url).call).to eq('/path/more?query=1&param=true')
+    end
+
+    it 'returns only the path if there is no query' do
+      url = 'https://example:80/path/more/path'
+
+      expect(Relativize.new(url).call).to eq('/path/more/path')
+    end
+  end
+
+  context 'given a relative URL' do
+    it 'returns the same path and query' do
+      url = '/path/more?query=1&param=true'
+
+      expect(Relativize.new(url).call).to eq('/path/more?query=1&param=true')
+    end
+
+    it 'returns the same path if there is no query' do
+      url = '/path/more/path'
+
+      expect(Relativize.new(url).call).to eq('/path/more/path')
+    end
+  end
+
+  context 'given a URL without path or query' do
+    it 'returns the relative URL after that port' do
+      url = 'https://www.example.com'
+
+      expect(Relativize.new(url).call).to eq('')
+    end
+  end
+
+  context 'given a malformed URL' do
+    it 'returns the provided default url' do
+      expect(Relativize.new(nil, fallback_url: '/def').call).to eq('/def')
+    end
+
+    it "returns '/' if no default url is provided" do
+      expect(Relativize.new(nil).call).to eq('/')
+    end
   end
 end
